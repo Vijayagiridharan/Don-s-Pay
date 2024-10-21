71e6a16e83823581fb547c98fb42ede2570e5918
Anshul Abrol <abroa01@pfw.edu>
2024-10-21 18:11:05 -0400

    Set up development environment for backend and frontend

---
+++ dons-pay-backend/src/test/java/com/acs560/dons_pay_backend/DonsPayBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acs560.dons_pay_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonsPayBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
